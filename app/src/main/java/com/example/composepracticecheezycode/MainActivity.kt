4c00fa6be46ee41c48e399fd6b7cf6b599e2f868
Suraj Pingale <surajpingale02@gmail.com>
2024-02-25 19:45:03 +0530

    second commit

--- app/src/main/java/com/example/composepracticecheezycode/MainActivity.kt
+++ app/src/main/java/com/example/composepracticecheezycode/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composepracticecheezycode.ui.theme.ComposePracticeCheezyCodeTheme
 
@@ -35,7 +36,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier,
-        fontStyle = FontStyle.Italic
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold
     )
 }
 
